61d7cc3239e3fe9364e4d81c923c48d7ca26a737
Nico Croce <nicoc123@gmail.com>
2018-10-15 12:07:25 -0300

    Se creó el primer GET. Retorna un json.

---
+++ 3-index.js
@@ -0,0 +1,36 @@
+// server.js
+
+// VARIABLES
+// ==============================================
+
+const express = require('express');
+const app     = express();
+const port    =   process.env.PORT || 1122;
+
+// RUTAS
+// ==============================================
+
+app.get('/', function(req, res) {
+    res.send('Prueba de API');  
+});
+
+app.get('/list', function(req, res) {
+    res.send({
+        taller: 'Api REST',
+        participantes: 10,
+        words: ['nodejs', 'js', 'express', 'server'] 
+    });  
+});
+
+// INICIAR SERVER
+// ==============================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
+console.log(`http://localhost:${port}`);
+
+
+/* 
+
+1) Agregar H1 a la respuesta.  
+
+*/
